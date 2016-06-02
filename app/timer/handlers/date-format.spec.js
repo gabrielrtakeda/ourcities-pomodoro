25319b66b425f838b129d6dc8a2c9602b20d7149
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-06-02 02:03:26 -0300

    Move features directory.

---
+++ app/timer/handlers/date-format.spec.js
@@ -0,0 +1,9 @@
+import expect from 'expect'
+import * as TimeConstants from './../constants/time'
+import * as DateFormatHandler from './date-format'
+
+describe('date-format', () => {
+  it('should return a string with format `mm:ss`', () => {
+    const timestamp = 25 * TimeConstants.MINUTE
+  })
+})
